78abb5700a81dbfcacd83d19b93e4f354cc64d24
carlos abramonte <car.abr.04@gmail.com>
2021-08-26 17:04:02 -0500

    nuevos cambios parte 2

--- src/app/ivercasa/footer/footer.component.ts
+++ src/app/ivercasa/footer/footer.component.ts
@@ -12,4 +12,22 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  lista:any [] = [
+    {"nombre":"ic@ivercasa.com"},
+    {"nombre":"https://www.ivercasa.com"},
+    {"nombre":"958 123 123 - "},
+    {"nombre":"Acera del Darro 10, 2ª planta Granada 18005"},
+    {"nombre":"(Granada)"},
+  ]
+
+  condic:any[] = [
+    {clase: "cond1", nombre:"Términos y condiciones"},
+    {clase: "cond2", nombre:"Política de privacidad"},
+    {clase: "cond3", nombre:"©2020 IVERCASA"},
+    {clase: "cond4", nombre:"Todos los derechos reservados"},
+  ]
+
+
+
 }
